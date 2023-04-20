d37d310ccf4a124574d8704482d89474e7683cd7
TPODAvia <thepowerofdarknes2000@gmail.com>
2023-04-20 10:36:48 +0300

    Merge branch 'master' of https://github.com/TPODAvia/Voice-Assistant

--- Irene-Voice-Assistant/plugins/plugin_tts_emotts.py
+++ Irene-Voice-Assistant/plugins/plugin_tts_emotts.py
@@ -5,9 +5,14 @@ import os
 import sys
 
 from vacore import VACore
-from vosk_tts.model import Model
-from vosk_tts.synth import Synth
 
+from tqdm import tqdm
+import torch
+
+sys.path.append("/home/vboxuser/Voice-Assistant/Multilingual_Text_to_Speech")
+from utilss import audio, text
+from utilss import build_model
+from params.params import Params as hp
 
 modname = os.path.basename(__file__)[:-3] # calculating modname
 
@@ -23,7 +28,7 @@ def start(core:VACore):
         # },
 
         "tts": {
-            "emotts": (init,say,None) # первая функция инициализации, вторая - говорить, третья - в wav file
+            "emotts": (init,None,towavfile) # первая функция инициализации, вторая - говорить, третья - в wav file
                                          # если вторая - None, то используется 3-я с проигрыванием файла
         }
     }
@@ -34,117 +39,99 @@ def init(core:VACore):
 
 def say(core:VACore, text_to_speech:str):
 
-    import librosa
-    import scipy
-    sys.path.append("/home/vboxuser/Voice-Assistant/Multilingual_Text_to_Speech")
-    from utilss import audio
-    from params.params import Params as hp
-    import numpy as np
-    import torch
-
-    checkpoint = "/home/vboxuser/Voice-Assistant/generated_switching.pyt"
-    torch.load(checkpoint, map_location="cpu")['parameters']
-    hp.decoder_language_init = False
-
-    model = build_model(checkpoint)
-    model.eval()
-
-    inputs = []
-    for s, l in [("09-ru","ru")]:
-        inputs += [
-            f"Совет обладает полномочиями по руководству всей операционной деятельностью Центрального банка и контролю за коммерческими оманскими банками.|{s}|{l}",
-        ]
-
-    generated_spectrograms = inference(model, inputs, "cpu")
-
-    for i, s in enumerate(generated_spectrograms):
-        s = audio.denormalize_spectrogram(s, not hp.predict_linear)
-        w = audio.inverse_spectrogram(s, not hp.predict_linear)
-        print(w, w.shape)
-
-        # write output
-        scipy.io.wavfile.write('test.wav', hp.sample_rate, np.array(w*4, dtype=np.float32))
-
-def towavfile(core:VACore, text_to_speech:str,wavfile:str):
     pass
 
-def build_model(checkpoint):
-    
-    import torch
-    from params.params import Params as hp
-    from utilss import remove_dataparallel_prefix
-    # from utilss.logging import Logger
-    from modules.tacotron2 import Tacotron
+def towavfile(core:VACore, text_to_speech:str,wavfile:str):
 
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    state = torch.load(checkpoint, map_location=device)
-    hp.load_state_dict(state['parameters'])
+    import argparse
 
-    model = Tacotron()
-    model_dict = model.state_dict()
-    pretrained_dict = remove_dataparallel_prefix(state['model'])
-    
-    missing = [k for k, _ in pretrained_dict.items() if k not in model_dict]
-    print(f'Missing model parts: {missing}')
-    
-    missing = [k for k, _ in model_dict.items() if k not in pretrained_dict]
-    print(f'Extra model parts: {missing}')
-    
-    pretrained_dict = {k: v for k, v in pretrained_dict.items() if k in model_dict}
-    
-    model_dict.update(pretrained_dict) 
-    model.load_state_dict(model_dict) 
-    model.to(device)
-    
-    return model
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--checkpoint", type=str, default="/home/vboxuser/Voice-Assistant/Multilingual_Text_to_Speech/checkpoints/generated_switching.pyt", help="Model checkpoint.")
+    parser.add_argument("--output", type=str, default=".", help="Path to output directory.")
+    parser.add_argument("--seed", type=int, default=None, help="Torch random seed.")
+    parser.add_argument("--cpu", action='store_true', help="Force to run on CPU.")
+    parser.add_argument("--save_spec", action='store_true', help="Saves also spectrograms if set.")
+    parser.add_argument("--ignore_wav", action='store_true', help="Does not save waveforms if set.")
+    args = parser.parse_args()
 
-def inference(model, inputs, device=None):
+    if args.seed is not None:
+        print(f"Random seed set to {args.seed}")
+        torch.manual_seed(args.seed)
 
-    import torch
-    from params.params import Params as hp
-    from utilss import text
+    print("Building model ...")
 
-    inputs = [l.rstrip().split('|') for l in inputs if l]
+    model = build_model(args.checkpoint, args.cpu)
+    model.eval()
+
+    #total_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+    #print(f"Builded model with {total_params} parameters")
+
+    inputs = ["04|" + text_to_speech + "|00-de|ru"]
+    progress = tqdm(enumerate(inputs), total=len(inputs), desc='Synthesizing')
 
     spectrograms = []
-    for i in inputs:
-        
-        clean_text = i[0]
+    for i, item in progress:
+        progress.set_postfix_str(item)
 
-        if not hp.use_punctuation: 
-            clean_text = text.remove_punctuation(clean_text)
-        if not hp.case_sensitive: 
-            clean_text = text.to_lower(clean_text)
-        if hp.remove_multiple_wspaces: 
-            clean_text = text.remove_odd_whitespaces(clean_text)
-        
-        t = torch.LongTensor(text.to_sequence(clean_text, use_phonemes=hp.use_phonemes))
-        
-        if hp.multi_language:     
-            l_tokens = i[2].split(',')
-            t_length = len(clean_text) + 1
-            l = []
-            for token in l_tokens:
-                l_d = token.split('-')
+        item_id = item.split("|")[0]
+        if item_id == "":
+            item_id = i
+
+        s = synthesize(model, item, args.cpu)
+
+        if not os.path.exists(args.output):
+            os.makedirs(args.output)
+
+        # if args.save_spec:
+        #     np.save(os.path.join(args.output, f'{item_id}.npy'), s)
+
+        if not args.ignore_wav:
+            w = audio.inverse_spectrogram(s, not hp.predict_linear)
+            audio.save(w, os.path.join(args.output, f'temp/vacore_1.wav'))
+
+    pass
+
+def synthesize(model, input_data, force_cpu=False):
+
+    item = input_data.split('|')
+    clean_text = item[1]
+
+    if not hp.use_punctuation: 
+        clean_text = text.remove_punctuation(clean_text)
+    if not hp.case_sensitive: 
+        clean_text = text.to_lower(clean_text)
+    if hp.remove_multiple_wspaces: 
+        clean_text = text.remove_odd_whitespaces(clean_text)
+
+    t = torch.LongTensor(text.to_sequence(clean_text, use_phonemes=hp.use_phonemes))
+
+    if hp.multi_language:     
+        l_tokens = item[3].split(',')
+        t_length = len(clean_text) + 1
+        l = []
+        for token in l_tokens:
+            l_d = token.split('-')
  
-                language = [0] * hp.language_number
-                for l_cw in l_d[0].split(':'):
-                    l_cw_s = l_cw.split('*')
-                    language[hp.languages.index(l_cw_s[0])] = 1 if len(l_cw_s) == 1 else float(l_cw_s[1])
-                language_length = (int(l_d[1]) if len(l_d) == 2 else t_length)
-                l += [language] * language_length
-                t_length -= language_length     
-            l = torch.FloatTensor([l])
-        else:
-            l = None
+            language = [0] * hp.language_number
+            for l_cw in l_d[0].split(':'):
+                l_cw_s = l_cw.split('*')
+                language[hp.languages.index(l_cw_s[0])] = 1 if len(l_cw_s) == 1 else float(l_cw_s[1])
 
-        s = torch.LongTensor([hp.unique_speakers.index(i[1])]) if hp.multi_speaker else None
+            language_length = (int(l_d[1]) if len(l_d) == 2 else t_length)
+            l += [language] * language_length
+            t_length -= language_length     
+        l = torch.FloatTensor([l])
+    else:
+        l = None
 
-        if device=="cuda" or (device is None and torch.cuda.is_available()): 
-            t = t.cuda(non_blocking=True)
-            if l: l = l.cuda(non_blocking=True)
-            if s: s = s.cuda(non_blocking=True)
-     
-        spectrograms.append(model.inference(t, speaker=s, language=l).cpu().detach().numpy())
+    s = torch.LongTensor([hp.unique_speakers.index(item[2])]) if hp.multi_speaker else None
 
-    return spectrograms
+    if torch.cuda.is_available() and not force_cpu: 
+        t = t.cuda(non_blocking=True)
+        if l is not None: l = l.cuda(non_blocking=True)
+        if s is not None: s = s.cuda(non_blocking=True)
+
+    s = model.inference(t, speaker=s, language=l).cpu().detach().numpy()
+    s = audio.denormalize_spectrogram(s, not hp.predict_linear)
+
+    return s
--- Irene-Voice-Assistant/runva_neuralnet.py
+++ Irene-Voice-Assistant/runva_neuralnet.py
@@ -1,120 +1,71 @@
-import argparse
-import os
-import queue
-import sounddevice as sd
-import vosk
-import sys
-import traceback
-import json
-from ..Face_ui import face
-sys.path.append("/home/vboxuser/Voice-Assistant/Audio_decision")
-import engine
+# you need to install:
+# pip install PyAudio
+# pip install SpeechRecognition
 
+# if you have problems with PyAudio install, check this:
+# https://github.com/EnjiRouz/Voice-Assistant-App/blob/master/README.md
+
+import speech_recognition
+import traceback
 from vacore import VACore
 
-mic_blocked = False
+# most from @EnjiRouz code: https://habr.com/ru/post/529590/
 
-def block_mic():
-    global mic_blocked
-    #print("Blocking microphone...")
-    mic_blocked = True
+
+def record_and_recognize_audio(*args: tuple):
+    """
+    Запись и распознавание аудио
+    """
+    with microphone:
+        recognized_data = ""
+
+        try:
+            #print("Listening...")
+            audio = recognizer.listen(microphone, 5, 5)
+            print("audio: ", microphone)
+
+        except speech_recognition.WaitTimeoutError:
+            print("Can you check if your microphone is on, please?")
+            return
+
+        # использование online-распознавания через Google
+        try:
+            #print("Started recognition...")
+            recognized_data = recognizer.recognize_google(audio, language="ru").lower()
+
+        except speech_recognition.UnknownValueError:
+            pass
+
+        # в случае проблем с доступом в Интернет происходит выброс ошибки
+        except speech_recognition.RequestError:
+            print("Check your Internet Connection, please")
+
+        return recognized_data
 
 # ------------------- vosk ------------------
 if __name__ == "__main__":
-    q = queue.Queue()
+    # инициализация инструментов распознавания и ввода речи
+    recognizer = speech_recognition.Recognizer()
+    microphone = speech_recognition.Microphone()
 
-    def int_or_str(text):
-        """Helper function for argument parsing."""
-        try:
-            return int(text)
-        except ValueError:
-            return text
+    with microphone:
+        # регулирование уровня окружающего шума
+        recognizer.adjust_for_ambient_noise(microphone, duration=2)
 
-    def audio_callback(indata, frames, time, status):
-        """This is called (from a separate thread) for each audio block."""
-        if status:
-            print(status, file=sys.stderr)
-        if not mic_blocked:
-            q.put(bytes(indata))
+    # initing core
+    core = VACore()
+    #core.init_plugin("core")
+    #core.init_plugins(["core"])
+    core.init_with_plugins()
 
-
-    parser = argparse.ArgumentParser(add_help=False)
-    parser.add_argument(
-        '-l', '--list-devices', action='store_true',
-        help='show list of audio devices and exit')
-    args, remaining = parser.parse_known_args()
-    if args.list_devices:
-        print(sd.query_devices())
-        parser.exit(0)
-    parser = argparse.ArgumentParser(
-        description=__doc__,
-        formatter_class=argparse.RawDescriptionHelpFormatter,
-        parents=[parser])
-    parser.add_argument(
-        '-r', '--samplerate', type=float, help='sampling rate of audio device')
-    parser.add_argument(
-        '-d', '--device', type=int_or_str,
-        help='input device (numeric ID or substring)')
-    parser.add_argument(
-        '-m', '--model', type=str, metavar='MODEL_PATH',
-        help='Path to the model')
-    args = parser.parse_args(remaining)
-
-    try:
-
-        if args.model is None:
-            args.model = "model"
-        if not os.path.exists(args.model):
-            print ("Please download a model for your language from https://alphacephei.com/vosk/models")
-            print ("and unpack as 'model' in the current folder.")
-            parser.exit(0)
-
-        if args.samplerate is None:
-            device_info = sd.query_devices(args.device, 'input')
-            args.samplerate = device_info['default_samplerate']
-
-        model = vosk.Model(args.model)
-
-        with sd.RawInputStream(samplerate=args.samplerate, blocksize = 8000, device=args.device, dtype='int16',
-                               channels=1, callback=audio_callback):
-            print('#' * 80)
-            print('Press Ctrl+C to stop the recording')
-            print('#' * 80)
-
-            rec = vosk.KaldiRecognizer(model, args.samplerate)
-
-            # initing core
-            core = VACore()
-            core.init_with_plugins()
-
-            while True:
-                data = q.get()
-
-                # engine.run(data)
-
-                if rec.AcceptWaveform(data):
-
-                    recognized_data = rec.Result()
-
-                    #print("1",recognized_data)
-
-                    #print(recognized_data)
-                    recognized_data = json.loads(recognized_data)
-                    #print(recognized_data)
-                    voice_input_str = recognized_data["text"]
+    while True:
+        # старт записи речи с последующим выводом распознанной речи
+        voice_input_str = record_and_recognize_audio()
 
 
-                    if voice_input_str != "":
-                        core.run_input_str(voice_input_str,block_mic)
-                        mic_blocked = False
 
-                else:
-                    #print("2",rec.PartialResult())
-                    pass
-                core._update_timers()
+        if voice_input_str != "":
+            core.run_input_str(voice_input_str)
 
-    except KeyboardInterrupt:
-        print('\nDone')
-        parser.exit(0)
-    except Exception as e:
-        parser.exit(type(e).__name__ + ': ' + str(e))
+
+        core._update_timers()
